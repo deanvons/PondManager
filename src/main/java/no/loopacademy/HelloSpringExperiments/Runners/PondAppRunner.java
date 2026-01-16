4f7bdd46eb27a5c55aaa96cbe415e472472b8994
Dr. Dean von Schoultz <deanvons@gmail.com>
2026-01-16 15:36:19 +0100

    FEAT: added controller to manage user input

--- src/main/java/no/loopacademy/HelloSpringExperiments/Runners/PondAppRunner.java
+++ src/main/java/no/loopacademy/HelloSpringExperiments/Runners/PondAppRunner.java
@@ -1,6 +1,7 @@
 package no.loopacademy.HelloSpringExperiments.Runners;
 
 
+import no.loopacademy.HelloSpringExperiments.Controllers.DuckConsoleController;
 import no.loopacademy.HelloSpringExperiments.DataAccess.DuckRepository;
 import no.loopacademy.HelloSpringExperiments.Services.DuckService;
 import org.springframework.boot.ApplicationArguments;
@@ -11,30 +12,14 @@ import org.springframework.stereotype.Component;
 // Also ensure you are storing classes you want Spring to scan in the same package as the spring applcation main
 @Component
 public class PondAppRunner implements ApplicationRunner {
-    private final DuckService duckService;
+    private final DuckConsoleController duckConsoleController;
 
-    public PondAppRunner(DuckService duckService){
-        this.duckService = duckService;
+    public PondAppRunner(DuckConsoleController duckConsoleController){
+        this.duckConsoleController = duckConsoleController;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Running......let's do some duck stuff");
-
-        // The application actually just runs methods that alter entity state via a DB, based on user interaction
-
-        // Check Duck list -> read from DB but what does the table look like
-
-        // now we can interact with the duck data in a way what feels like a business process
-        var duckList = duckService.listDucks();
-
-        duckList.forEach(duck ->  System.out.println(duck.getNickName()));
-
-        // this area of our application is essentially where we decide what code to run based on the user needs / input
-
-        // business rules exist only in the services
-
-        // We need a class/layer dedicated to buser responses (CONTROLLER)
-
+        duckConsoleController.start();
     }
 }
